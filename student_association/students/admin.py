4d352d463f1c342b95cdfaebe6bb9eff32edbffd
héric Libong <hericlibong@gmail.com>
2025-03-16 20:03:02 +0100

    create admin interface

--- student_association/students/admin.py
+++ student_association/students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Discipline, Student, Club, Membership
 
-# Register your models here.
+admin.site.register(Discipline)
+admin.site.register(Student)    
+admin.site.register(Club)
+admin.site.register(Membership)
